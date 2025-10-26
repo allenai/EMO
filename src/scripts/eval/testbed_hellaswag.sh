6e45d1d3ab7cd0427bf98438e736c79191dafea8
ryanyxw <ryanywan@usc.edu>
2025-10-26 12:00:30 -0700

    update path

--- src/scripts/eval/testbed_hellaswag.sh
+++ src/scripts/eval/testbed_hellaswag.sh
@@ -1,9 +1,9 @@
 python -u -m offline_evals.run_eval\
     --task '{"task_name": "hellaswag", "split": "validation", "primary_metric": "acc_per_char", "num_shots": 5, "limit": 1000, "fewshot_source": "OLMES:hellaswag", "metadata": {"regimes": ["OLMES-v0.1"], "alias": "hellaswag:rc::olmes"}}'\
     --batch-size 4 \
-    --output-dir /root/phdbrainstorm/evals/testbed_hellaswag \
+    --output-dir /root/ryanwang/phdbrainstorm/evals/testbed_hellaswag \
     --save-raw-requests true \
     --num-workers 1 \
     --gpus 1 \
-    --model olmo2_1b-pretrain-mose-natural-1025/step30995-hf \
+    --model /root/ryanwang/phdbrainstorm/models/olmo2_1b-pretrain-mose-natural-1025/step30995-hf \
     --model-args '{"model_type": "hf"}'
