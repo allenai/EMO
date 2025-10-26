c309bd837ce32eb515cb7cb091ebd1ebb1b1f81d
ryanyxw <ryanywan@usc.edu>
2025-10-26 14:08:31 -0700

    update

--- src/scripts/eval/testbed_hellaswag.sh
+++ src/scripts/eval/testbed_hellaswag.sh
@@ -2,6 +2,7 @@ python -u -m offline_evals.run_eval\
     --task '{"task_name": "hellaswag", "split": "validation", "primary_metric": "acc_per_char", "num_shots": 5, "limit": 1000, "fewshot_source": "OLMES:hellaswag", "metadata": {"regimes": ["OLMES-v0.1"], "alias": "hellaswag:rc::olmes"}}'\
     --batch-size 4 \
     --output-dir /root/ryanwang/phdbrainstorm/evals/testbed_hellaswag \
+    --recompute_metrics true \
     --save-raw-requests true \
     --num-workers 1 \
     --gpus 1 \
