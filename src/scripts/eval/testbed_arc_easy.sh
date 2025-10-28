59829ecf70fa8c1bb4c1301304ea460957889674
ryanyxw <ryanywan@usc.edu>
2025-10-28 12:37:45 -0700

    update

--- src/scripts/eval/testbed_arc_easy.sh
+++ src/scripts/eval/testbed_arc_easy.sh
@@ -1,7 +1,7 @@
 python -m offline_evals.run_eval \
-    --task '{"task_name": "arc_easy", "split": "test", "primary_metric": "acc_per_char", "num_shots": 5, "limit": 1000, "fewshot_source": "OLMES:ARC-Easy", "metadata": {"description": "ARC-Easy (RC) using OLMES-v0.1", "regimes": ["OLMES-v0.1"], "alias": "arc_easy:rc::olmes"}}' \
+    --task '{"task_name": "arc_easy:mc", "split": "test", "num_shots": 5, "limit": 1000, "fewshot_source": "OLMES:ARC-Easy", "metadata": {"description": "ARC-Easy (MC) using OLMES-v0.1", "regimes": ["OLMES-v0.1"], "alias": "arc_easy:mc::olmes"}}' \
     --batch-size 4 \
-    --output-dir /root/ryanwang/phdbrainstorm/evals/testbed_arc_easy \
+    --output-dir /weka/oe-training-default/ryanwang/phdbrainstorm/evals/weka_oe-training-default_ryanwang_phdbrainstorm_models_olmoe-pretrain-mose-unbalanced-1012_step30995-hf \
     --save-raw-requests true \
     --num-workers 1 \
     --gpus 1 \
