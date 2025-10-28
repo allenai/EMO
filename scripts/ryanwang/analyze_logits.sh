e3cd30e209f16bea95cd2345f0d7a9d07457d0a0
ryanyxw <ryanywan@usc.edu>
2025-10-28 14:37:54 -0700

    update

--- scripts/ryanwang/analyze_logits.sh
+++ scripts/ryanwang/analyze_logits.sh
@@ -4,11 +4,11 @@
 # Usage: bash src/scripts/eval/launch_beaker_eval.sh
 
 # Configuration
-MODEL_DIRS=("/weka/oe-training-default/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-natural-1022/step30995-hf")
-EVAL_DIRS="/weka/oe-training-default/ryanwang/phdbrainstorm/evals/weka_oe-training-default_ryanwang_phdbrainstorm_models_olmoe-pretrain-mose-natural-1022_step30995-hf"
+#MODEL_DIRS=("/weka/oe-training-default/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-natural-1022/step30995-hf")
+#EVAL_DIRS="/weka/oe-training-default/ryanwang/phdbrainstorm/evals/weka_oe-training-default_ryanwang_phdbrainstorm_models_olmoe-pretrain-mose-natural-1022_step30995-hf"
 
-#MODEL_DIRS=("/root/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-natural-1022/step30995-hf")
-#EVAL_DIRS=("/root/ryanwang/phdbrainstorm/evals/weka_oe-training-default_ryanwang_phdbrainstorm_models_olmoe-pretrain-mose-natural-1022_step30995-hf")
+MODEL_DIRS=("/root/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-natural-1022/step30995-hf")
+EVAL_DIRS=("/root/ryanwang/phdbrainstorm/evals/weka_oe-training-default_ryanwang_phdbrainstorm_models_olmoe-pretrain-mose-natural-1022_step30995-hf")
 BATCH_SIZE=16
 GPUS=1
 CLUSTER="ai2/jupiter-cirrascale-2"
@@ -113,35 +113,35 @@ for i in "${!MODEL_DIRS[@]}"; do
         echo "  Batch size: $BATCH_SIZE"
         echo "  Job name: $job_name"
 
-#        PYTHONPATH=. python -u src/scripts/eval/launch_logits.py \
-#            --model "$MODEL_PATH" \
-#            --task "$TASK" \
-#            --eval-dir "$EVAL_DIR" \
-#            --batch-size "$BATCH_SIZE" \
-#            --gpus "$GPUS" \
-#            --use_correct_only
+        PYTHONPATH=. python -u src/scripts/eval/launch_logits.py \
+            --model "$MODEL_PATH" \
+            --task "$TASK" \
+            --eval-dir "$EVAL_DIR" \
+            --batch-size "$BATCH_SIZE" \
+            --gpus "$GPUS" \
+            --use_correct_only
 
-        gantry run \
-            --name $job_name \
-            --weka oe-training-default:/weka/oe-training-default \
-            --install "bash src/scripts/eval/setup_eval_env.sh;" \
-            --budget ai2/oceo \
-            --workspace ai2/flex2 \
-            --cluster $CLUSTER \
-            --priority urgent \
-            --gpus $GPUS \
-            --env-secret HF_TOKEN=RYAN_HF_TOKEN \
-            --env-secret AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID \
-            --env-secret AWS_SECRET_ACCESS_KEY=RYAN_AWS_SECRET_ACCESS_KEY \
-            -- \
-            bash -c "PYTHONPATH=. python -u src/scripts/eval/launch_logits.py \
-                --model $MODEL_PATH \
-                --task $TASK \
-                --eval-dir "$EVAL_DIR" \
-                --batch-size "$BATCH_SIZE" \
-                --gpus "$GPUS" \
-                --use_correct_only
-                "
+#        gantry run \
+#            --name $job_name \
+#            --weka oe-training-default:/weka/oe-training-default \
+#            --install "bash src/scripts/eval/setup_eval_env.sh;" \
+#            --budget ai2/oceo \
+#            --workspace ai2/flex2 \
+#            --cluster $CLUSTER \
+#            --priority urgent \
+#            --gpus $GPUS \
+#            --env-secret HF_TOKEN=RYAN_HF_TOKEN \
+#            --env-secret AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID \
+#            --env-secret AWS_SECRET_ACCESS_KEY=RYAN_AWS_SECRET_ACCESS_KEY \
+#            -- \
+#            bash -c "PYTHONPATH=. python -u src/scripts/eval/launch_logits.py \
+#                --model $MODEL_PATH \
+#                --task $TASK \
+#                --eval-dir "$EVAL_DIR" \
+#                --batch-size "$BATCH_SIZE" \
+#                --gpus "$GPUS" \
+#                --use_correct_only
+#                "
         echo "Launched evaluation for model: $model, task: $TASK"
         echo "----------------------------------------"
     done
