b87141dc5b6900365e9732b78ee358f5c2a6edba
ryanyxw <ryanywan@usc.edu>
2025-10-28 14:20:28 -0700

    BEGIN FORMAL ARC_EASY

--- scripts/ryanwang/launch_beaker_eval.sh
+++ scripts/ryanwang/launch_beaker_eval.sh
@@ -14,32 +14,33 @@ model_type=hf
 # Define all available tasks from run_eval.sh (ALL tasks from all groups)
 TASKS=(
     # MC9 tasks
-    arc_easy:mc::olmes
-    arc_challenge:mc::olmes
-    boolq:mc::olmes
-    csqa:mc::olmes
-    hellaswag:mc::olmes
-    openbookqa:mc::olmes
-    piqa:mc::olmes
-    socialiqa:mc::olmes
-    winogrande:mc::olmes
-
-    arc_easy:rc::olmes
-    arc_challenge:rc::olmes
-    boolq:rc::olmes
-    csqa:rc::olmes
-    hellaswag:rc::olmes
-    openbookqa:rc::olmes
-    piqa:rc::olmes
-    socialiqa:rc::olmes
-    winogrande:rc::olmes
-
-    # Gen5 tasks
-    coqa::olmes
-    squad::olmes
-    naturalqs::olmes
-    triviaqa::olmes
-    drop::olmes
+    arc_easy:mc_validation::olmes
+#    arc_easy:mc::olmes
+#    arc_challenge:mc::olmes
+#    boolq:mc::olmes
+#    csqa:mc::olmes
+#    hellaswag:mc::olmes
+#    openbookqa:mc::olmes
+#    piqa:mc::olmes
+#    socialiqa:mc::olmes
+#    winogrande:mc::olmes
+#
+#    arc_easy:rc::olmes
+#    arc_challenge:rc::olmes
+#    boolq:rc::olmes
+#    csqa:rc::olmes
+#    hellaswag:rc::olmes
+#    openbookqa:rc::olmes
+#    piqa:rc::olmes
+#    socialiqa:rc::olmes
+#    winogrande:rc::olmes
+#
+#    # Gen5 tasks
+#    coqa::olmes
+#    squad::olmes
+#    naturalqs::olmes
+#    triviaqa::olmes
+#    drop::olmes
 #
     # MMLU tasks
 #    mmlu:mc::olmes
