e0d234a364f4b33727ba7aced988fce43dc58849
ryanyxw <ryanywan@usc.edu>
2025-10-28 10:04:33 -0700

    update

--- scripts/ryanwang/train_olmoe_finetune.sh
+++ scripts/ryanwang/train_olmoe_finetune.sh
@@ -25,7 +25,7 @@ activation_file="/weka/oe-training-default/ryanwang/phdbrainstorm/evals/weka_oe-
 
 runname="olmoe-finetune-${task}"
 
-torchrun --nproc-per-node=4 src/scripts/train/olmoe-1B-7B_finetune.py \
+torchrun --nproc-per-node=1 src/scripts/train/olmoe-1B-7B_finetune.py \
     $runname \
 		--save-folder="${base_model}/$runname" \
 		--dataset.mix=arc-easy-train \
