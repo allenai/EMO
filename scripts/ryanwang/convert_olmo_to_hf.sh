c95f07997fcaccc1bf12dfe31df6392d3b3ac3ae
ryanyxw <ryanywan@usc.edu>
2025-10-29 09:02:23 -0700

    update

--- scripts/ryanwang/convert_olmo_to_hf.sh
+++ scripts/ryanwang/convert_olmo_to_hf.sh
@@ -2,5 +2,5 @@
 python src/examples/huggingface/convert_checkpoint_to_hf.py \
   --checkpoint-input-path /root/ryanwang/phdbrainstorm/models/dense_1b_olmoe-mix_1028/step30995 \
   --max-sequence-length 4096 \
-  --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/dense_1b_olmoe-mix_1028/step30995 \
+  --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/dense_1b_olmoe-mix_1028/step30995-hf \
   --dtype float32
