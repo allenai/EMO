282d02a77dc071063991156d045717a1e55e589b
ryanyxw <ryanywan@usc.edu>
2025-10-28 17:34:42 -0700

    refactor code and launch dense_1b_olmoe-mix_1028

--- scripts/ryanwang/models/dense_1b_olmoe-mix_1028.sh
+++ scripts/ryanwang/models/dense_1b_olmoe-mix_1028.sh
@@ -14,17 +14,12 @@
 #       `--trainer.hard_stop.value=100 --trainer.hard_stop.unit=steps`
 
 ##############################################################
-#git config --global credential.helper '!f() { echo "username=ryanyxw"; echo "password=$GH_TOKEN}"; }; f'
-# print out the current working directory
-command='''
-cd /weka && ls -al
-'''
 
-runname="test-beaker-secrets"
+runname="dense_1b_olmoe-mix_1028"
 python -m olmo_core.launch.beaker \
   --name $runname \
-	--gpus 1 \
-  --nodes 1 \
+	--gpus 8 \
+  --nodes 8 \
   --is_private_repo \
 	--weka=oe-training-default \
   --shared-filesystem \
@@ -34,8 +29,15 @@ python -m olmo_core.launch.beaker \
 	--allow-dirty \
 	--priority urgent \
 	--env-secret "GITHUB_TOKEN=RYAN_GITHUB_TOKEN" "WANDB_API_KEY=RYAN_WANDB_API_KEY" "BEAKER_TOKEN=RYAN_BEAKER_TOKEN" "AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY=RYAN_AWS_SECRET_ACCESS_KEY" "HF_TOKEN=RYAN_HF_TOKEN" "BEAKER_TOKEN=RYAN_BEAKER_TOKEN" \
-	-- \
-	bash -c "$command"
+	-- src/scripts/train/olmo2-1B.py \
+    $runname \
+		--save-folder="/weka/oe-training-default/ryanwang/phdbrainstorm/models/$runname" \
+		--dataset.mix=OLMoE-mix-0824 \
+		--work-dir="/weka/oe-training-default/ryanwang/dataset-cache" \
+		--trainer.max_duration='{value: 130_000_000_000, unit: tokens}' \
+		--trainer.callbacks.wandb="{enabled: true, entity: ryanyxw, project: olmoe-modular, name: ${runname}}" \
+
+
 
 
 
