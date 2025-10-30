5df10a6934af5671810a1d714b304dc948a39840
ryanyxw <ryanywan@usc.edu>
2025-10-30 15:54:24 -0700

    update

--- scripts/ryanwang/launch_beaker_eval.sh
+++ scripts/ryanwang/launch_beaker_eval.sh
@@ -162,7 +162,7 @@ for MODEL_PATH in "${MODELS[@]}"; do
             --budget ai2/oe-base \
             --workspace ai2/flex2 \
             --cluster $CLUSTER \
-            --priority high \
+            --priority urgent \
             --gpus $gpus \
             --env-secret HF_TOKEN=RYAN_HF_TOKEN \
             --env-secret AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID \
