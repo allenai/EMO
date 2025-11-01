c1010060de66b810420b4dc3120164e830e36fe1
ryanyxw <ryanywan@usc.edu>
2025-10-31 21:23:05 -0700

    update

--- scripts/ryanwang/launch_beaker_eval.sh
+++ scripts/ryanwang/launch_beaker_eval.sh
@@ -4,11 +4,11 @@
 # Usage: bash src/scripts/eval/launch_beaker_eval.sh
 
 # Configuration
-#MODEL_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE/models
-MODEL_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/models"
+MODEL_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE/models
+#MODEL_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/models"
 MODELS=("dense_1b_olmoe-mix_1028/step30995-hf")
-#BASE_OUTPUT_DIR="s3://ai2-sewonm/ryanwang/evals"
-BASE_OUTPUT_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/evals"
+BASE_OUTPUT_DIR="s3://ai2-sewonm/ryanwang/evals"
+#BASE_OUTPUT_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/evals"
 BATCH_SIZE=16
 CLUSTER="ai2/jupiter-cirrascale-2"
 model_type=hf
@@ -19,7 +19,7 @@ TASK_GROUPS_LIST=(
 #  "arc_challenge|arc_challenge:mc_train::olmes arc_challenge:mc_validation::olmes arc_challenge:mc_test::olmes arc_challenge:rc_train::olmes arc_challenge:rc_validation::olmes arc_challenge:rc_test::olmes"
 #  "boolq|boolq:mc_train::olmes boolq:mc_validation::olmes boolq:mc_test::olmes boolq:rc_train::olmes boolq:rc_validation::olmes boolq:rc_test::olmes"
 #  "csqa|csqa:mc_train::olmes csqa:mc_validation::olmes csqa:mc_test::olmes csqa:rc_train::olmes csqa:rc_validation::olmes csqa:rc_test::olmes"
-#  "hellaswag|hellaswag:mc_train::olmes hellaswag:mc_validation::olmes hellaswag:mc_test::olmes hellaswag:rc_train::olmes hellaswag:rc_validation::olmes hellaswag:rc_test::olmes"
+  "hellaswag|hellaswag:mc_train::olmes hellaswag:mc_validation::olmes hellaswag:mc_test::olmes hellaswag:rc_train::olmes hellaswag:rc_validation::olmes hellaswag:rc_test::olmes"
 #  "openbookqa|openbookqa:mc_train::olmes openbookqa:mc_validation::olmes openbookqa:mc_test::olmes openbookqa:rc_train::olmes openbookqa:rc_validation::olmes openbookqa:rc_test::olmes"
 #  "piqa|piqa:mc_train::olmes piqa:mc_validation::olmes piqa:mc_test::olmes piqa:rc_train::olmes piqa:rc_validation::olmes piqa:rc_test::olmes"
 #  "socialiqa|socialiqa:mc_train::olmes socialiqa:mc_validation::olmes socialiqa:mc_test::olmes socialiqa:rc_train::olmes socialiqa:rc_validation::olmes socialiqa:rc_test::olmes"
@@ -32,9 +32,7 @@ TASK_GROUPS_LIST=(
 #  "mmlu_rc_valid_test|mmlu:rc_validation::olmes mmlu:rc_test::olmes"
 #
 #  # Gen5 tasks
-#  "gen5|coqa::olmes squad::olmes naturalqs::olmes triviaqa::olmes drop::olmes"
-#  squad::olmes
-  "test|hellaswag:mc_train::olmes"
+  "gen5|coqa::olmes squad::olmes naturalqs::olmes triviaqa::olmes drop::olmes"
 
 )
 
@@ -161,35 +159,35 @@ for MODEL_PATH in "${MODELS[@]}"; do
         echo "  Batch size: $batch_size"
         echo "  Job name: $job_name"
 
-        PYTHONPATH=. python -u src/scripts/eval/launch_eval.py \
-                --model "${MODEL_DIR}/${MODEL_PATH}" \
-                --model-type hf \
-                --task $TASK \
-                --output-dir $OUTPUT_DIR \
-                --batch-size $batch_size \
-                --gpus $gpus \
-
-#        gantry run \
-#            --name $job_name \
-#            --weka oe-training-default:/weka/oe-training-default \
-#            --install "pip install -e \".[all]\"" \
-#            --budget ai2/oe-base \
-#            --workspace ai2/flex2 \
-#            --cluster $CLUSTER \
-#            --priority urgent \
-#            --gpus $gpus \
-#            --env-secret HF_TOKEN=RYAN_HF_TOKEN \
-#            --env-secret AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID \
-#            --env-secret AWS_SECRET_ACCESS_KEY=RYAN_AWS_SECRET_ACCESS_KEY \
-#            -- \
-#            bash -c "PYTHONPATH=. python -u src/scripts/eval/launch_eval.py \
+#        PYTHONPATH=. python -u src/scripts/eval/launch_eval.py \
 #                --model "${MODEL_DIR}/${MODEL_PATH}" \
 #                --model-type hf \
 #                --task $TASK \
-#                --remote-output-dir $OUTPUT_DIR \
+#                --output-dir $OUTPUT_DIR \
 #                --batch-size $batch_size \
 #                --gpus $gpus \
-#                "
+
+        gantry run \
+            --name $job_name \
+            --weka oe-training-default:/weka/oe-training-default \
+            --install "pip install -e \".[all]\"" \
+            --budget ai2/oe-base \
+            --workspace ai2/flex2 \
+            --cluster $CLUSTER \
+            --priority urgent \
+            --gpus $gpus \
+            --env-secret HF_TOKEN=RYAN_HF_TOKEN \
+            --env-secret AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID \
+            --env-secret AWS_SECRET_ACCESS_KEY=RYAN_AWS_SECRET_ACCESS_KEY \
+            -- \
+            bash -c "PYTHONPATH=. python -u src/scripts/eval/launch_eval.py \
+                --model "${MODEL_DIR}/${MODEL_PATH}" \
+                --model-type hf \
+                --task $TASK \
+                --remote-output-dir $OUTPUT_DIR \
+                --batch-size $batch_size \
+                --gpus $gpus \
+                "
 
         echo "Launched evaluation for model: $model, group: $GROUP_NAME"
         echo "----------------------------------------"
