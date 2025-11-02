5e6d80956e7e09c5f0ff2b0cd36ed56e82007571
ryanyxw <ryanywan@usc.edu>
2025-11-02 10:22:37 -0800

    added conversion path fix

--- scripts/ryanwang/convert_olmo_to_hf.sh
+++ scripts/ryanwang/convert_olmo_to_hf.sh
@@ -1,6 +1,6 @@
 
 python src/examples/huggingface/convert_checkpoint_to_hf.py \
-  --checkpoint-input-path /root/ryanwang/phdbrainstorm/models/moe_1b7b_olmoe-mix_300B_1030/step71526 \
+  --checkpoint-input-path /root/ryanwang/phdbrainstorm/FlexMoE/models/moe_1b7b_olmoe-mix_300B_1030/step71526 \
   --max-sequence-length 4096 \
-  --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/moe_1b7b_olmoe-mix_300B_1030/step71526-hf \
+  --huggingface-output-dir /root/ryanwang/phdbrainstorm/FlexMoE/models/moe_1b7b_olmoe-mix_300B_1030/step71526-hf \
   --dtype float32
