c1ff94b7ef71f5c09651af779f3146ee5d223663
ryanyxw <ryanywan@usc.edu>
2025-11-03 09:23:21 -0800

    test conversion of all checkpoints

--- scripts/ryanwang/convert_olmo_to_hf.sh
+++ scripts/ryanwang/convert_olmo_to_hf.sh
@@ -1,6 +1,24 @@
+BASE_FOLDER="/root/ryanwang/phdbrainstorm/FlexMoE/models"
+MODELS=(
+  "dense_1b_olmoe-mix_300B_1030"
+)
 
-python src/examples/huggingface/convert_checkpoint_to_hf.py \
-  --checkpoint-input-path /root/ryanwang/phdbrainstorm/FlexMoE/models/dense_1b_olmoe-mix_300B_1030/step71526 \
-  --max-sequence-length 4096 \
-  --huggingface-output-dir /root/ryanwang/phdbrainstorm/FlexMoE/models/dense_1b_olmoe-mix_300B_1030/step71526-hf \
-  --dtype float32
+for MODEL in "${MODELS[@]}"; do
+  # list all the files in the model directory, only include directories that start with "step"
+  MODEL_DIR="${BASE_FOLDER}/${MODEL}"
+  STEPS=$(ls -d ${MODEL_DIR}/step*/ | xargs -n 1 basename)
+  for STEP in ${STEPS}; do
+    # check that the hf converted folder does not already exist
+    if [ -d "${MODEL_DIR}/${STEP}-hf" ]; then
+      break 1
+    fi
+    echo "Converting model ${MODEL} at step ${STEP} to Huggingface format..."
+
+    echo "checkpoint_input_path: ${MODEL_DIR}/${STEP}"
+    echo "huggingface_output_dir: ${MODEL_DIR}/${STEP}-hf"
+
+#    python src/examples/huggingface/convert_checkpoint_to_hf.py \
+#      --checkpoint-input-path "${MODEL_DIR}/${STEP}" \
+#      --max-sequence-length 4096 \
+#      --huggingface-output-dir "${MODEL_DIR}/${STEP}-hf" \
+#      --dtype float32
