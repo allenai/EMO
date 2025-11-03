21e568a72a7d0585815de2b13b4d70febf5bd1f1
ryanyxw <ryanywan@usc.edu>
2025-11-03 10:57:56 -0800

    update bug in num_experts path

--- scripts/ryanwang/models/moe_1b7b_128experts_olmoe-mix_130B_1103.sh
+++ scripts/ryanwang/models/moe_1b7b_128experts_olmoe-mix_130B_1103.sh
@@ -36,7 +36,7 @@ python -m olmo_core.launch.beaker \
 		--work-dir="/weka/oe-training-default/ryanwang/dataset-cache" \
 		--trainer.max_duration='{value: 130_000_000_000, unit: tokens}' \
 		--trainer.callbacks.wandb="{enabled: true, entity: ryanyxw, project: olmoe-modular, name: ${runname}}" \
-		--model.feed_forward_moe.num_experts=128 \
+		--model.block.feed_forward_moe.num_experts=128 \
 
 
 
