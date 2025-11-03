eb155c331c0f29d55389d599cb499633abfa15b9
ryanyxw <ryanywan@usc.edu>
2025-11-03 09:25:49 -0800

    push conversion script completed

--- scripts/ryanwang/convert_olmo_to_hf.sh
+++ scripts/ryanwang/convert_olmo_to_hf.sh
@@ -1,6 +1,7 @@
 BASE_FOLDER="/root/ryanwang/phdbrainstorm/FlexMoE/models"
 MODELS=(
   "dense_1b_olmoe-mix_300B_1030"
+  "moe_1b7b_olmoe-mix_300B_1030"
 )
 
 for MODEL in "${MODELS[@]}"; do
@@ -14,14 +15,12 @@ for MODEL in "${MODELS[@]}"; do
     fi
     echo "Converting model ${MODEL} at step ${STEP} to Huggingface format..."
 
-    echo "checkpoint_input_path: ${MODEL_DIR}/${STEP}"
-    echo "huggingface_output_dir: ${MODEL_DIR}/${STEP}-hf"
+    python src/examples/huggingface/convert_checkpoint_to_hf.py \
+      --checkpoint-input-path "${MODEL_DIR}/${STEP}" \
+      --max-sequence-length 4096 \
+      --huggingface-output-dir "${MODEL_DIR}/${STEP}-hf" \
+      --dtype float32
 
   done
 done
 
-#    python src/examples/huggingface/convert_checkpoint_to_hf.py \
-#      --checkpoint-input-path "${MODEL_DIR}/${STEP}" \
-#      --max-sequence-length 4096 \
-#      --huggingface-output-dir "${MODEL_DIR}/${STEP}-hf" \
-#      --dtype float32
