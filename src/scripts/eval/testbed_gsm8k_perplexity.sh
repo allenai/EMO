105df6d0ed03f41fa550579ffe9a4d6204040d11
ryanyxw <ryanywan@usc.edu>
2025-11-05 17:33:47 -0800

    update

--- src/scripts/eval/testbed_gsm8k_perplexity.sh
+++ src/scripts/eval/testbed_gsm8k_perplexity.sh
@@ -1,5 +1,5 @@
 python -m offline_evals.run_eval \
-    --task '{"task_name": "gsm8k:perplexity", "split": "test", "primary_metric": "bits_per_byte", "num_shots": 8, "fewshot_source": "STD:GSM8k", "metadata": {"regimes": ["OLMES-v0.2"]}}' \
+    --task '{"task_name": "gsm8k:perplexity_train", "split": "test", "primary_metric": "bits_per_byte", "num_shots": 8, "fewshot_source": "STD:GSM8k", "metadata": {"regimes": ["OLMES-v0.2"]}}' \
     --batch-size 16 \
     --output-dir /root/ryanwang/phdbrainstorm/FlexMoE/evals/dense_1b_olmoe-mix_300B_1030_step71526-hf \
     --save-raw-requests true \
