a3ec59d14d465b7868ad5bb91ddb71d1f2a3ed0b
ryanyxw <ryanywan@usc.edu>
2025-11-07 09:41:06 -0800

    update

--- scripts/ryanwang/tokenize.sh
+++ scripts/ryanwang/tokenize.sh
@@ -16,20 +16,20 @@ get_eval_filename() {
 }
 train_task_names=(
   "arc_easy:rc_train_0shot::olmes"
-  "arc_challenge:rc_train_0shot::olmes"
-  "boolq:rc_train_0shot::olmes"
-  "csqa:rc_train_0shot::olmes"
-  "hellaswag:rc_train_0shot::olmes"
-  "openbookqa:rc_train_0shot::olmes"
-  "piqa:rc_train_0shot::olmes"
-  "socialiqa:rc_train_0shot::olmes"
-  "winogrande:rc_train_0shot::olmes"
-
-#   MMLU
-  "mmlu_rc:rc_train_0shot::olmes"
-
-#   GSM8K
-  "gsm8k:perplexity_train_0shot::olmes"
+#  "arc_challenge:rc_train_0shot::olmes"
+#  "boolq:rc_train_0shot::olmes"
+#  "csqa:rc_train_0shot::olmes"
+#  "hellaswag:rc_train_0shot::olmes"
+#  "openbookqa:rc_train_0shot::olmes"
+#  "piqa:rc_train_0shot::olmes"
+#  "socialiqa:rc_train_0shot::olmes"
+#  "winogrande:rc_train_0shot::olmes"
+#
+##   MMLU
+#  "mmlu_rc:rc_train_0shot::olmes"
+#
+##   GSM8K
+#  "gsm8k:perplexity_train_0shot::olmes"
 )
 
 for train_task_name in "${train_task_names[@]}"; do
@@ -44,24 +44,25 @@ for train_task_name in "${train_task_names[@]}"; do
     tokenizer_name="allenai/OLMo-2-1124-7B"
     jsonl_file="${BASE_OUTPUT_DIR}/${processed_train_file}"
     destination="${BASE_OUTPUT_DIR}/${task_prefix}-tokenized"
+    echo "destination folder: $destination"
 
-    # gzip the data if not already gzipped
-    if [[ ! -f "${jsonl_file}.gz" ]]; then
-      echo "Gzipping ${jsonl_file}..."
-      gzip ${jsonl_file}
-    else
-      echo "${jsonl_file}.gz already exists. Skipping gzip."
-    fi
-
-    # tokenize the files
-    dolma tokens \
-      --documents ${jsonl_file}.gz \
-      --tokenizer.name_or_path ${tokenizer_name} \
-      --tokenizer.eos_token_id 100257 \
-      --tokenizer.pad_token_id 100277 \
-      --destination ${destination} \
-      --dtype uint32 \
-      --processes 1
+#    # gzip the data if not already gzipped
+#    if [[ ! -f "${jsonl_file}.gz" ]]; then
+#      echo "Gzipping ${jsonl_file}..."
+#      gzip ${jsonl_file}
+#    else
+#      echo "${jsonl_file}.gz already exists. Skipping gzip."
+#    fi
+#
+#    # tokenize the files
+#    dolma tokens \
+#      --documents ${jsonl_file}.gz \
+#      --tokenizer.name_or_path ${tokenizer_name} \
+#      --tokenizer.eos_token_id 100257 \
+#      --tokenizer.pad_token_id 100277 \
+#      --destination ${destination} \
+#      --dtype uint32 \
+#      --processes 1
 
 done
 
