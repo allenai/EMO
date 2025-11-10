1adfd34cba0c5bc8e6602ffadd8058e91cbbc32d
ryanyxw <ryanywan@usc.edu>
2025-11-09 22:42:08 -0800

    temp removed forward backward dry run to test forward pass

--- src/olmo_core/train/trainer.py
+++ src/olmo_core/train/trainer.py
@@ -1310,7 +1310,7 @@ class Trainer:
             return  # for backwards compatibility
 
         log.info("Starting forward/backward dry-run batch...")
-        self.train_module.train_batch(batch, dry_run=True)
+        # self.train_module.train_batch(batch, dry_run=True)
         log.info("Dry-run complete")
 
     def _fit_epoch(self):
