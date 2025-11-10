6197fa308e667268a7285af40e3116495cc400c5
ryanyxw <ryanywan@usc.edu>
2025-11-10 15:53:46 -0800

    update

--- scripts/ryanwang/models/twolevel_1b7b_128experts_olmoe-mix_130B_1110.sh
+++ scripts/ryanwang/models/twolevel_1b7b_128experts_olmoe-mix_130B_1110.sh
@@ -26,8 +26,8 @@ torchrun --nproc-per-node=1 src/scripts/train/olmoe-1B-7B_fsl.py \
 		--trainer.max_duration='{value: 130_000_000_000, unit: tokens}' \
 		--trainer.callbacks.wandb="{enabled: true, entity: ryanyxw, project: olmoe-modular, name: ${runname}}" \
 		--model.block.feed_forward_moe.num_experts=16 \
-		--dataset.generate_doc_lengths=true \
-		--model.block.attention.backend=flash_2 \
+#		--dataset.generate_doc_lengths=true \
+#		--model.block.attention.backend=flash_2 \
 		--model-type="twolevel" \
 		--document-expert-pool=${document_expert_pool} \
 
