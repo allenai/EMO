aea3c327504b3f9da255210cd50b136dbacbbed6
ryanyxw <ryanywan@usc.edu>
2025-11-10 16:07:50 -0800

    update

--- src/scripts/train/olmoe-1B-7B_fsl.py
+++ src/scripts/train/olmoe-1B-7B_fsl.py
@@ -56,7 +56,9 @@ log = logging.getLogger(__name__)
 DATA_ROOT = "/root/ryanwang"
 
 SEQUENCE_LENGTH = 4096
-GLOBAL_BATCH_SIZE = 1024 * SEQUENCE_LENGTH
+#HACK
+GLOBAL_BATCH_SIZE = 4 * SEQUENCE_LENGTH
+# GLOBAL_BATCH_SIZE = 1024 * SEQUENCE_LENGTH
 
 
 # docs: start-define-config
