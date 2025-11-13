4906fd7bc3dd01804e10d352f161ac84cf55aef5
ryanyxw <ryanywan@usc.edu>
2025-11-13 02:12:10 -0800

    update

--- src/scripts/eval/launch_logits_random.py
+++ src/scripts/eval/launch_logits_random.py
@@ -5,8 +5,16 @@ sample_file = "/root/ryanwang/phdbrainstorm/FlexMoE/prune/moe_1b7b_128experts_ol
 
 # load the sample file and print the first line
 import json
+import torch
 
 with open(sample_file, "r") as f:
     logits = f.readline()
     logits_json = json.loads(logits)
+    logits_values = logits_json["avg_router_probabilities"]
+
+    logits_tensor = torch.tensor(logits_values)
+
+    # create a random tensor with the same shape as logits_tensor
+    random_tensor = torch.rand_like(logits_tensor)
+
     breakpoint()
