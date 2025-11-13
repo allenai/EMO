67304bb36399e3eba94b5a1ec12d7ca5cd14549d
ryanyxw <ryanywan@usc.edu>
2025-11-12 16:39:24 -0800

    update

--- scripts/ryanwang/pruning/launch_beaker_eval_pruned.sh
+++ scripts/ryanwang/pruning/launch_beaker_eval_pruned.sh
@@ -132,7 +132,7 @@ for PARENT_MODEL in "${PARENT_MODELS[@]}"; do
             --name $job_name \
             --weka oe-training-default:/weka/oe-training-default \
             --install "pip install -e \".[all]\"" \
-            --budget ai2/oe-oceo \
+            --budget ai2/oceo \
             --workspace ai2/flex2 \
             --cluster $CLUSTER \
             --priority urgent \
