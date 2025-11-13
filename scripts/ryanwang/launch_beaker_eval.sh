cbe6129bb3f12d23a105da83a99251fbdee1d2b8
ryanyxw <ryanywan@usc.edu>
2025-11-12 22:30:22 -0800

    update

--- scripts/ryanwang/launch_beaker_eval.sh
+++ scripts/ryanwang/launch_beaker_eval.sh
@@ -7,11 +7,12 @@
 MODEL_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE/models
 #MODEL_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/models"
 MODELS=(
-    "moe_1b7b_128experts_olmoe-mix_130B_1103/step30995-hf"
-    "dense_1b_olmoe-mix_300B_1030/step71526-hf"
-    "dense_1b_olmoe-mix_1028/step30995-hf"
-    "moe_1b7b_olmoe-mix_300B_1030/step71526-hf"
-    "moe_1b7b_olmoe-mix/step30995-hf"
+    "twolevel-32_1b7b_128experts_olmoe-mix_130B_1110/step30995-hf"
+#    "moe_1b7b_128experts_olmoe-mix_130B_1103/step30995-hf"
+#    "dense_1b_olmoe-mix_300B_1030/step71526-hf"
+#    "dense_1b_olmoe-mix_1028/step30995-hf"
+#    "moe_1b7b_olmoe-mix_300B_1030/step71526-hf"
+#    "moe_1b7b_olmoe-mix/step30995-hf"
     )
 #MODELS=(
 #    "dense_1b_olmoe-mix_300B_1030/step0-hf"
@@ -41,25 +42,25 @@ model_type=hf
 TASK_GROUPS_LIST=(
   ######### TEST-only ##########
   # MC9 tasks
-#  "arc_easy|arc_easy:mc_test::olmes arc_easy:rc_test::olmes"
-#  "arc_challenge|arc_challenge:mc_test::olmes arc_challenge:rc_test::olmes"
-#  "boolq|boolq:mc_test::olmes boolq:rc_test::olmes"
-#  "csqa|csqa:mc_test::olmes csqa:rc_test::olmes"
-#  "hellaswag|hellaswag:mc_test::olmes hellaswag:rc_test::olmes"
-#  "openbookqa|openbookqa:mc_test::olmes openbookqa:rc_test::olmes"
-#  "piqa|piqa:mc_test::olmes piqa:rc_test::olmes"
-#  "socialiqa|socialiqa:mc_test::olmes socialiqa:rc_test::olmes"
+  "arc_easy|arc_easy:mc_test::olmes arc_easy:rc_test::olmes"
+  "arc_challenge|arc_challenge:mc_test::olmes arc_challenge:rc_test::olmes"
+  "boolq|boolq:mc_test::olmes boolq:rc_test::olmes"
+  "csqa|csqa:mc_test::olmes csqa:rc_test::olmes"
+  "hellaswag|hellaswag:mc_test::olmes hellaswag:rc_test::olmes"
+  "openbookqa|openbookqa:mc_test::olmes openbookqa:rc_test::olmes"
+  "piqa|piqa:mc_test::olmes piqa:rc_test::olmes"
+  "socialiqa|socialiqa:mc_test::olmes socialiqa:rc_test::olmes"
   "winogrande|winogrande:mc_test::olmes winogrande:rc_test::olmes"
 
 #   MMLU
-#  "mmlu_mc_test|mmlu:mc_test::olmes"
-#  "mmlu_rc_test|mmlu:rc_test::olmes"
+  "mmlu_mc_test|mmlu:mc_test::olmes"
+  "mmlu_rc_test|mmlu:rc_test::olmes"
 
 #   Gen5 tasks
-#  "gen5|coqa::olmes squad::olmes naturalqs::olmes triviaqa::olmes drop::olmes"
+  "gen5|coqa::olmes squad::olmes naturalqs::olmes triviaqa::olmes drop::olmes"
 
 #   GSM8K
-#  "gsm8k_test|gsm8k:perplexity_test::olmes"
+  "gsm8k_test|gsm8k:perplexity_test::olmes"
 
   ######### TRAIN-VAL-TEST ##########
 #  # MC9 tasks
