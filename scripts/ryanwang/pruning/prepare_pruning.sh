4087fa080c434dd27f5a2a9544e10ff4a493a44a
ryanyxw <ryanywan@usc.edu>
2025-11-17 22:47:45 -0800

    added keepk-8 for finetuning to compare against base model

--- scripts/ryanwang/pruning/prepare_pruning.sh
+++ scripts/ryanwang/pruning/prepare_pruning.sh
@@ -3,9 +3,9 @@
 MODEL_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE/models
 #MODEL_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/models"
 MODELS=(
-#    "twolevelbatchlb-32_1b14b_stability_filter-true_zlossweight-1e-3_1115/step30995-hf"
+    "twolevelbatchlb-32_1b14b_stability_filter-true_zlossweight-1e-3_1115/step30995-hf"
 #    "twolevel-32_1b7b_128experts_olmoe-mix_130B_1110/step30995-hf"
-    "moe_1b7b_128experts_olmoe-mix_130B_1103/step30995-hf"
+#    "moe_1b7b_128experts_olmoe-mix_130B_1103/step30995-hf"
 #    "moe_1b7b_olmoe-mix/step30995-hf"
     )
 #BASE_OUTPUT_DIR="s3://ai2-sewonm/ryanwang/prune"
