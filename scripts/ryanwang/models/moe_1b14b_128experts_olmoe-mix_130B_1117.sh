0c08300f4b5981d00d5dfbba8bc755de1de8baa5
ryanyxw <ryanywan@usc.edu>
2025-11-18 08:55:29 -0800

    added model-type

--- scripts/ryanwang/models/moe_1b14b_128experts_olmoe-mix_130B_1117.sh
+++ scripts/ryanwang/models/moe_1b14b_128experts_olmoe-mix_130B_1117.sh
@@ -36,6 +36,7 @@ python -m olmo_core.launch.beaker \
 		--work-dir="/weka/oe-training-default/ryanwang/dataset-cache" \
 		--trainer.max_duration='{value: 130_000_000_000, unit: tokens}' \
 		--trainer.callbacks.wandb="{enabled: true, entity: ryanyxw, project: olmoe-modular, name: ${runname}}" \
+		--model-type="moe" \
 		--model.block.feed_forward_moe.num_experts=128 \
 		--dataset.instance_filter_config='{repetition_max_period: 13, repetition_min_period: 1, repetition_max_count: 32}' \
 
