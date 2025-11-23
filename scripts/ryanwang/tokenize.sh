f657e725fb1293b7e2f15cc36d36b1b8efb70f53
ryanyxw <ryanywan@usc.edu>
2025-11-23 15:34:31 -0800

    update

--- scripts/ryanwang/tokenize.sh
+++ scripts/ryanwang/tokenize.sh
@@ -18,14 +18,14 @@ get_eval_filename() {
 }
 train_task_names=(
   "arc_easy:rc_train::olmes"
-  "arc_challenge:rc_train::olmes"
-  "boolq:rc_train::olmes"
-  "csqa:rc_train::olmes"
-  "hellaswag:rc_train::olmes"
-  "openbookqa:rc_train::olmes"
-  "piqa:rc_train::olmes"
-  "socialiqa:rc_train::olmes"
-  "winogrande:rc_train::olmes"
+#  "arc_challenge:rc_train::olmes"
+#  "boolq:rc_train::olmes"
+#  "csqa:rc_train::olmes"
+#  "hellaswag:rc_train::olmes"
+#  "openbookqa:rc_train::olmes"
+#  "piqa:rc_train::olmes"
+#  "socialiqa:rc_train::olmes"
+#  "winogrande:rc_train::olmes"
 
 
 #  "arc_easy:rc_train_0shot::olmes"
@@ -68,14 +68,14 @@ for train_task_name in "${train_task_names[@]}"; do
     fi
 
     # tokenize the files
-    dolma tokens \
-      --documents ${jsonl_file}.gz \
-      --tokenizer.name_or_path ${tokenizer_name} \
-      --tokenizer.eos_token_id 100257 \
-      --tokenizer.pad_token_id 100277 \
-      --destination ${destination} \
-      --dtype uint32 \
-      --processes 1
+#    dolma tokens \
+#      --documents ${jsonl_file}.gz \
+#      --tokenizer.name_or_path ${tokenizer_name} \
+#      --tokenizer.eos_token_id 100257 \
+#      --tokenizer.pad_token_id 100277 \
+#      --destination ${destination} \
+#      --dtype uint32 \
+#      --processes 1
 
     # we next add the label masks
     files=($(ls ${destination}/*.npy | grep -v mask.npy))
