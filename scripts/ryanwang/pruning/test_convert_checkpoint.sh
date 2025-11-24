86136524b410378400bd6a6623625bb01df9c190
ryanyxw <ryanywan@usc.edu>
2025-11-23 20:50:50 -0800

    update

--- scripts/ryanwang/pruning/test_convert_checkpoint.sh
+++ scripts/ryanwang/pruning/test_convert_checkpoint.sh
@@ -5,7 +5,7 @@ MODEL_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/models/twolevelbatchlb-32_1b14b_
 SAVE_PATH="/root/ryanwang/phdbrainstorm/FlexMoE/models/twolevelbatchlb-32_1b14b_stability_filter-true_zlossweight-1e-3_1115/step30995_keepk32"
 
 python src/scripts/eval/prune_moe_checkpoint.py \
-  --model_dir "$MODEL_DIR" \
+  --checkpoint_path "$MODEL_DIR" \
   --save_path "$SAVE_PATH" \
   --prune_keep_k "$prune_keep_k"
 
