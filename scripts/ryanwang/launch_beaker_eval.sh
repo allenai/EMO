bd0eeff4b0e52caf4a9aaa85fa988fc4edf44822
ryanyxw <ryanywan@usc.edu>
2025-11-27 01:49:46 -0800

    first working version of conversion with noqknorm and prenorm

--- scripts/ryanwang/launch_beaker_eval.sh
+++ scripts/ryanwang/launch_beaker_eval.sh
@@ -7,9 +7,10 @@
 MODEL_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE/models
 #MODEL_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/models"
 MODELS=(
-    "moe_1b14b_128experts_olmoe-mix_130B_1117/step30995-hf"
-    "dense_1b_olmoe-mix_1119/step30995-hf"
-    "twolevelbatchlb-32_1b14b_stability_filter-true_zlossweight-1e-3_1115/step30995-hf"
+#    "moe_1b14b_128experts_olmoe-mix_130B_1117/step30995-hf"
+#    "dense_1b_olmoe-mix_1119/step30995-hf"
+#    "twolevelbatchlb-32_1b14b_stability_filter-true_zlossweight-1e-3_1115/step30995-hf"
+    "moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123/step30995-hf"
 
 #    "twolevelsamplingnolb-32_1b14b_stability_filter-true_zlossweight-1e-3_1116/step30995-hf"
 #    "twolevelbatchlb-32_1b14b_stability_filter-true_zlossweight-1e-3_1115/step30995-hf"
