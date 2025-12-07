30bbc0ac4e44f3a0c7fb0bb954fde4f5355c545c
ryanyxw <ryanywan@usc.edu>
2025-12-07 13:21:15 -0800

    update

--- scripts/ryanwang/pruning/finetune_without_pruning.sh
+++ scripts/ryanwang/pruning/finetune_without_pruning.sh
@@ -24,7 +24,7 @@ num_checkpoints=5
 
 # this is used for ablations
 variation="newdefault_lr-4e-5_batchsize-16"
-expertiment_tag="newdefault_ablations_openbookqa"
+expertiment_tag="newdefault_ablations_hellaswag"
 
 variation_flags=""
 # Define variation-specific settings
@@ -62,8 +62,8 @@ train_task_names=(
 #  "arc_challenge:rc_train::olmes"
 #  "boolq:rc_train::olmes"
 #  "csqa:rc_train::olmes"
-#  "hellaswag:rc_train::olmes"
-  "openbookqa:rc_train::olmes"
+  "hellaswag:rc_train::olmes"
+#  "openbookqa:rc_train::olmes"
 #  "piqa:rc_train::olmes"
 #  "socialiqa:rc_train::olmes"
 #  "winogrande:rc_train::olmes"
