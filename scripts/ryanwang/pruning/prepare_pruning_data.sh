8024a46511a9219242b5121365c358c7e0fd01d2
ryanyxw <ryanywan@usc.edu>
2025-12-09 10:26:50 -0800

    update

--- scripts/ryanwang/pruning/prepare_pruning_data.sh
+++ scripts/ryanwang/pruning/prepare_pruning_data.sh
@@ -9,10 +9,12 @@ MODELS=(
 
 #    "twolevelbatchlb-32_1b14b_stability_prenorm_noqknorm_1121/step30995-hf"
 #    "twolevelbatchlb-32_1b14b_stability_lr-6e-4_1203/step30995-hf"
+    "twolevelbatchlb-8_1b7b_stability_1207/step30995-hf"
+
 #    "moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123/step30995-hf"
 #    "twolevelsamplingnolb-32_1b14b_stability_1127/step30995-hf"
 
-    "mutualinfo_1b14b_cond-1e-2_uncond-1e-2_1205/step30995-hf"
+#    "mutualinfo_1b14b_cond-1e-2_uncond-1e-2_1205/step30995-hf"
     )
 #BASE_OUTPUT_DIR="s3://ai2-sewonm/ryanwang/prune"
 #BASE_OUTPUT_DIR="/root/ryanwang/phdbrainstorm/FlexMoE/prune"
