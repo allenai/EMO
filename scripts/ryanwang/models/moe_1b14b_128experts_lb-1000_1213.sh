0d45c836a2ede31fffd2d70eeeb522bd18f9e65d
ryanyxw <ryanywan@usc.edu>
2025-12-13 15:02:09 -0800

    update

---
+++ scripts/ryanwang/models/moe_1b14b_128experts_lb-1000_1213.sh
@@ -0,0 +1,43 @@
+# PARENT:"moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123.sh"
+# DESCRIPTION:
+#     - Same as parent except increased the lb loss weight to 1000 to debug what happens with less experts
+# STATUS: USED
+##############################################################
+
+runname="moe_1b14b_128experts_lb-1000_1213"
+python -m olmo_core.launch.beaker \
+  --name $runname \
+	--gpus 8 \
+  --nodes 16 \
+	--weka=oe-training-default \
+  --shared-filesystem \
+	--workspace ai2/flex2 \
+	--cluster ai2/jupiter \
+  --is_private_repo \
+	--preemptible \
+	--allow-dirty \
+	--priority urgent \
+	--env-secret "GITHUB_TOKEN=RYAN_GITHUB_TOKEN" "WANDB_API_KEY=RYAN_WANDB_API_KEY" "BEAKER_TOKEN=RYAN_BEAKER_TOKEN" "AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY=RYAN_AWS_SECRET_ACCESS_KEY" "HF_TOKEN=RYAN_HF_TOKEN" \
+	-- src/scripts/train/olmoe-1B-7B_fsl.py \
+    $runname \
+		--save-folder="/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE/models/$runname" \
+		--dataset.mix=OLMoE-mix-0824 \
+		--work-dir="/weka/oe-training-default/ryanwang/dataset-cache" \
+		--trainer.max_duration='{value: 130_000_000_000, unit: tokens}' \
+		--trainer.callbacks.wandb="{enabled: true, entity: ryanyxw, project: olmoe-modular, name: ${runname}, tags: [pretraining]}" \
+		--model-type="moe" \
+		--model.block.feed_forward_moe.num_experts=128 \
+		--dataset.instance_filter_config='{repetition_max_period: 13, repetition_min_period: 1, repetition_max_count: 32}' \
+		--model.block.name="moe" \
+		--model.block.attention.qk_norm=null \
+		--model.block.feed_forward_moe.lb_loss_weight=1000
+
+
+
+
+
+
+
+
+
+
