a82758edcb59ac3c3f52a799bf3d975bf3de2b06
ryanyxw <ryanywan@usc.edu>
2025-12-18 21:28:33 +0800

    update synthea testbed

--- src/scripts/eval/testbed_synthea.sh
+++ src/scripts/eval/testbed_synthea.sh
@@ -6,5 +6,5 @@ python -m offline_evals.run_eval \
     --save-raw-requests true \
     --num-workers 1 \
     --gpus 1 \
-    --model /root/ryanwang/phdbrainstorm/models/dense_1b_olmoe-mix_prenorm_noqknorm_1123/step30995-hf \
+    --model /root/ryanwang/phdbrainstorm/FLexMoE/models/dense_1b_olmoe-mix_prenorm_noqknorm_1123/step30995-hf \
     --model-args '{"model_type": "hf"}'
