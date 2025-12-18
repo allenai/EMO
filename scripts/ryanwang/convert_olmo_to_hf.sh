3c2bc6daaa8e81f853c19d832a45f9d2c3c5ecaf
ryanyxw <ryanywan@usc.edu>
2025-12-19 07:25:35 +0800

    update

--- scripts/ryanwang/convert_olmo_to_hf.sh
+++ scripts/ryanwang/convert_olmo_to_hf.sh
@@ -10,13 +10,13 @@ PARENT_MODELS=(
 
 #    "twolevelbatchlb-32_1b14b_stability_filter-true_zlossweight-1e-3_1115/step30995"
 
-    "twolevelbatchlb-32_1b14b_stability_prenorm_noqknorm_1121/step30995"
+#    "twolevelbatchlb-32_1b14b_stability_prenorm_noqknorm_1121/step30995"
 #    "twolevelbatchlb-32_1b14b_stability_lr-6e-4_1203/step30995"
 #    "twolevelbatchlb-8_1b7b_stability_1207/step30995"
 
 #    "mutualinfo_1b14b_cond-1e-2_uncond-1e-2_1205/step30995"
 
-    "moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123/step30995"
+#    "moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123/step30995"
 #    "twolevelsamplingnolb-32_1b14b_stability_1127/step30995"
     "dense_1b_olmoe-mix_prenorm_noqknorm_1123/step30995"
 )
@@ -24,7 +24,7 @@ PARENT_MODELS=(
 # used to iterate on different finetuning variations
 #postfix=""
 #postfix="_keepk32/newdefault_lr-4e-5"
-postfix="_keepk32/newdefault_lr-4e-6_bs-128"
+postfix="_keepk32/newdefault_lr-4e-5_bs-128"
 #postfix="_keepk8/newdefault_lr-4e-5"
 #postfix="_keepk32"
 #postfix="_keepk32/lr-3e-5_warmup-0.2"
