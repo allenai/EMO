c1d8053333be511daae8942b277fb853e318fde7
Akshita Bhagia <akshita23bhagia@gmail.com>
2025-12-19 00:14:05 -0800

    fix datamix

--- src/olmo_core/data/mixes/__init__.py
+++ src/olmo_core/data/mixes/__init__.py
@@ -43,6 +43,8 @@ class DataMix(DataMixBase):
     OLMo_mix_0925 = "OLMo-mix-0925"
     OLMo_mix_0625_150Bsample = "OLMo-mix-0625-150Bsample"
 
+    mj_finemath4plus = "mj_finemath4plus"
+
     @classmethod
     def _missing_(cls, value: object) -> "DataMix | None":
         """Handle alias lookups."""
