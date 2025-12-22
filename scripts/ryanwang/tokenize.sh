73f68933b7d5610a13e7e9c6fcda7339acab39d6
ryanyxw <ryanywan@usc.edu>
2025-12-22 21:04:07 +0800

    update

--- scripts/ryanwang/tokenize.sh
+++ scripts/ryanwang/tokenize.sh
@@ -27,7 +27,8 @@ train_task_names=(
 #  "socialiqa:rc_train::olmes"
 #  "winogrande:rc_train::olmes"
 #  "synthea:rc_train_0shot::olmes"
-  "gsm8k_generation:train_0shot::olmes"
+#  "gsm8k_generation:train_0shot::olmes"
+  "coqa:train_0shot::olmes"
 
 
 #  "arc_easy:rc_train_0shot::olmes"
