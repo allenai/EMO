2645a7fab120e6a9da9f8e686db062866b8d46cd
ryanyxw <ryanywan@usc.edu>
2025-12-23 14:00:41 +0800

    update

--- scripts/ryanwang/pruning/prepare_pruning_data.sh
+++ scripts/ryanwang/pruning/prepare_pruning_data.sh
@@ -103,9 +103,9 @@ for MODEL_PATH in "${MODELS[@]}"; do
         fi
 
 #        # if the model is a 35b model, further reduce batch size by half
-#        if [[ $MODEL_PATH == *"1b35b"* ]]; then
-#            batch_size=$((batch_size / 2))
-#        fi
+        if [[ $MODEL_PATH == *"1b35b"* ]]; then
+            batch_size=$((batch_size / 2))
+        fi
 
         # adjust number of gpus requested if its mmlu, agi_eval, bbh, gsm8k, minerva, codex, mbpp
         if [[ $TASK == *mmlu* || $TASK == *agi_eval* || $TASK == *bbh* || $TASK == *gsm8k* || $TASK == *minerva_math_* || $TASK == *codex* || $TASK == *mbpp* ]]; then
