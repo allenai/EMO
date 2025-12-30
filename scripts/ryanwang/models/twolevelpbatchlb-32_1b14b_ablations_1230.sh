5fea2b8f51e9df7da877cc32b26595c2cf84be80
ryanyxw <ryanywan@usc.edu>
2025-12-30 09:24:10 +0900

    update

--- scripts/ryanwang/models/twolevelpbatchlb-32_1b14b_ablations_1230.sh
+++ scripts/ryanwang/models/twolevelpbatchlb-32_1b14b_ablations_1230.sh
@@ -5,8 +5,8 @@
 ##############################################################
 document_expert_pool=32
 #lb_loss_weight=0.01
-lb_loss_weight=0
-uncond_lb_bias=1e-1
+lb_loss_weight=1e-2
+uncond_lb_bias=1e-2
 uncond_bias=0
 
 runname="twolevelpbatchlb-${document_expert_pool}_1b14b_uncond-${uncond_bias}_uncondlb-${uncond_lb_bias}_lbloss-${lb_loss_weight}_1230"
