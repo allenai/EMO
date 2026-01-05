52ab161b1637ef8408cc8392329f893e3a226031
ryanyxw <ryanywan@usc.edu>
2026-01-05 16:19:25 -0500

    update

--- scripts/ryanwang/pruning/launch_beaker_eval_pruned.sh
+++ scripts/ryanwang/pruning/launch_beaker_eval_pruned.sh
@@ -152,8 +152,12 @@ TASK_GROUPS_LIST=(
 #  "winogrande|winogrande:rc_test::olmes"
 #  "gsm8k_generation|gsm8k_generation:test_0shot::olmes"
 #  "synthea|synthea:rc_test_0shot::olmes"
-  "coqa|coqa:test_0shot::olmes"
+#  "coqa|coqa:test_0shot::olmes"
 #  "squad|squad:test_0shot::olmes"
+
+
+  "coqa|coqa:train_0shot::olmes"
+
 
 #   MMLU
 #  "mmlu_rc_test|mmlu:rc_test::olmes"
