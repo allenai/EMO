8cf65b65f0c37831a7e4be5f3913c84197ba5a14
ryanyxw <ryanywan@usc.edu>
2026-01-16 18:02:43 -0800

    update

--- scripts/ryanwang/models_0116/moe_1b14b_lr-ablations_0116.sh
+++ scripts/ryanwang/models_0116/moe_1b14b_lr-ablations_0116.sh
@@ -3,7 +3,7 @@
 #     - Same as parent except cleaned up name, set learning rate to default
 # STATUS: USED
 ##############################################################
-lr=4e-4
+lr=4e-3
 
 runname="moe_1b14b_lr-${lr}-ablations_0116"
 python -m olmo_core.launch.beaker \
