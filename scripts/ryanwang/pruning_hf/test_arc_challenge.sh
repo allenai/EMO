0587c8def3db57d99feac583839e463ab9d65c55
ryanyxw <ryanywan@usc.edu>
2026-01-22 15:12:31 -0800

    begin testing hf_finetune

--- scripts/ryanwang/pruning_hf/test_arc_challenge.sh
+++ scripts/ryanwang/pruning_hf/test_arc_challenge.sh
@@ -1,6 +1,6 @@
 scripts/hf_finetune_with_pruning.sh \
-   --model /root/ryanwang/phdbrainstorm/FlexMoE/models/dense_1b_olmoe-mix_prenorm_noqknorm_1123/step30995-hf \
+   --model /root/ryanwang/phdbrainstorm/FlexMoE/models/moe_1b4b_32experts_1224/step30995-hf \
    --task arc_challenge \
-   --prune-keep-k 32 \
+   --prune-keep-k 16 \
    --output-dir /root/ryanwang/phdbrainstorm/evals/testbed_arc_challenge \
    --num-gpus 1
