d7a3fdf67f4c29da105b6eea89318bbcf59c637a
Kevin Farhat <kevinfarhat@allenai.org>
2026-01-22 14:11:03 -0800

    remove cuda launch blocking

--- src/scripts/kevinf/train/launch_olmo3_1b-olmoe-mix-0824-cc.sh
+++ src/scripts/kevinf/train/launch_olmo3_1b-olmoe-mix-0824-cc.sh
@@ -22,7 +22,7 @@ python -m olmo_core.launch.beaker \
   --preemptible \
   --allow-dirty \
   --env-secret "GITHUB_TOKEN=KEVINF_GITHUB_TOKEN" "WANDB_API_KEY=KEVINF_WANDB_API_KEY" "BEAKER_TOKEN=KEVINF_BEAKER_TOKEN" "AWS_ACCESS_KEY_ID=KEVINF_AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY=KEVINF_AWS_SECRET_ACCESS_KEY" \
-  --env "S3_PROFILE=" "CUDA_LAUNCH_BLOCKING=1" \
+  --env "S3_PROFILE=" \
   -- src/scripts/kevinf/train/OLMo3-1B.py \
   $runname \
   --save-folder="/weka/oe-training-default/kevinf/checkpoints-new/${runname}/" \
