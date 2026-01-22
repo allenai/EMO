a3b12eb01ff514a0702d3b2e5caf6ba9f8215b7a
Kevin Farhat <kevinfarhat@allenai.org>
2026-01-21 16:46:26 -0800

    add init for new dataset

--- src/olmo_core/data/mixes/__init__.py
+++ src/olmo_core/data/mixes/__init__.py
@@ -43,6 +43,7 @@ class DataMix(DataMixBase):
     OLMo_mix_0925 = "OLMo-mix-0925"
     OLMo_mix_0625_150Bsample = "OLMo-mix-0625-150Bsample"
     OLMo_mix_0625_150Bsample_dclm = "OLMo-mix-0625-150Bsample-dclm"
+    OLMoE_mix_0824_cc = "OLMoE-mix-0824-cc"
 
     mj_finemath4plus = "mj_finemath4plus"
     mose_code_mix = "mose_code_mix"
