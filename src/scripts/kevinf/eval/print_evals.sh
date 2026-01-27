555bdb66b29ddab1115881ab149ec0f6fe3d4ca2
Kevin Farhat <kevinfarhat@allenai.org>
2026-01-27 11:59:54 -0800

    example eval results script

---
+++ src/scripts/kevinf/eval/print_evals.sh
@@ -0,0 +1,9 @@
+python src/scripts/eval/print_evals.py \
+    -b /data/input/kevinf/flexmoe/eval/results/ \
+    -r \
+    -t \
+    --show-models "chem" \
+    --hide-models "" \
+    --show-tasks "" \
+    --hide-tasks "" \
+    --avg-all \
