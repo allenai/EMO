8e69aa42d9fc41ff5f99628045e2bd02800aef16
ryanyxw <ryanywan@usc.edu>
2026-01-29 23:00:17 -0800

    finished logmoecallback

--- src/hf_training/LogMoECallback.py
+++ src/hf_training/LogMoECallback.py
@@ -24,9 +24,7 @@ class LogMoeCallback(TrainerCallback):
         self._latest_lb_loss = None  # tensor
         self._latest_ce_loss = None  # tensor
         self._window_lb_sum = None  # tensor
-        self._window_lb_count = 0   # python int
         self._window_ce_sum = None  # tensor
-        self._window_ce_count = 0   # python int
 
     def _extract_aux_loss(self, output, aux_loss_key):
         aux = getattr(output, aux_loss_key, None)
@@ -43,7 +41,7 @@ class LogMoeCallback(TrainerCallback):
         # add on model's router_aux_loss_coef scaling since that is done in the trainer
         router_aux_loss_coef = module.config.router_aux_loss_coef
         lb_loss = lb_loss * router_aux_loss_coef
-        ce_loss = ce_loss * router_aux_loss_coef
+        ce_loss = ce_loss
 
         # stash *latest* value only (avoid checkpoint recompute double counting)
         if lb_loss is not None:
@@ -58,9 +56,7 @@ class LogMoeCallback(TrainerCallback):
         m = model.module if hasattr(model, "module") else model
         self._handle = m.register_forward_hook(self._forward_hook)
         self._window_lb_sum = torch.tensor(0.0, device=args.device)
-        self._window_lb_count = 0
         self._window_ce_sum = torch.tensor(0.0, device=args.device)
-        self._window_ce_count = 0
 
     def on_train_end(self, args, state, control, **kwargs):
         breakpoint()
@@ -71,19 +67,15 @@ class LogMoeCallback(TrainerCallback):
         self._latest_lb_loss = None
         self._latest_ce_loss = None
         self._window_lb_sum = None
-        self._window_lb_count = 0
         self._window_ce_sum = None
-        self._window_ce_count = 0
 
     def _accumulate_latest(self):
         if self._latest_lb_loss is not None:
             self._window_lb_sum += self._latest_lb_loss.float().to(self._window_lb_sum.device)
-            self._window_lb_count += 1
             self._latest_lb_loss = None
 
         if self._latest_ce_loss is not None:
             self._window_ce_sum += self._latest_ce_loss.float().to(self._window_ce_sum.device)
-            self._window_ce_count += 1
             self._latest_ce_loss = None
 
     def on_substep_end(self, args, state, control, **kwargs):
@@ -97,29 +89,24 @@ class LogMoeCallback(TrainerCallback):
     def on_log(self, args, state, control, logs=None, **kwargs):
         breakpoint()
 
-        if logs is not None and self._window_lb_count != 0:
+        if logs is not None:
             sum_t_lb = self._window_lb_sum
-            count_t_lb = torch.tensor(float(self._window_lb_count), device=sum_t_lb.device)
 
             if self.reduce_across_processes and dist.is_available() and dist.is_initialized():
                 dist.all_reduce(sum_t_lb, op=dist.ReduceOp.SUM)
-                dist.all_reduce(count_t_lb, op=dist.ReduceOp.SUM)
 
-            logs[f"train/{self.lb_loss_key}"] = (sum_t_lb / count_t_lb).item()
+            logs[f"train/{self.lb_loss_key}"] = sum_t_lb.item()
+            # note: we do not need to divide by anything, since this has been taken care of by num_items_in_batch
 
             self._window_lb_sum.zero_()
-            self._window_lb_count = 0
 
-        if logs is not None and self._window_ce_count != 0:
+        if logs is not None:
             sum_t_ce = self._window_ce_sum
-            count_t_ce = torch.tensor(float(self._window_ce_count), device=sum_t_ce.device)
 
             if self.reduce_across_processes and dist.is_available() and dist.is_initialized():
                 dist.all_reduce(sum_t_ce, op=dist.ReduceOp.SUM)
-                dist.all_reduce(count_t_ce, op=dist.ReduceOp.SUM)
 
-            logs[f"train/{self.ce_loss_key}"] = (sum_t_ce / count_t_ce).item()
+            logs[f"train/{self.ce_loss_key}"] = sum_t_ce.item()
 
             self._window_ce_sum.zero_()
-            self._window_ce_count = 0
         return
