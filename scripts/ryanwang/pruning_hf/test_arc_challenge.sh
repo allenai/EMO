0621943077673a939825376247f39b6c21918c09
ryanyxw <ryanywan@usc.edu>
2026-02-02 23:47:00 -0800

    working pipeline

--- scripts/ryanwang/pruning_hf/test_arc_challenge.sh
+++ scripts/ryanwang/pruning_hf/test_arc_challenge.sh
@@ -30,10 +30,10 @@ TASK_GROUPS_LIST=(
 #  "piqa"
 #  "socialiqa"
 #  "winogrande"
-  "gsm8k_generation_0shot"
+#  "gsm8k_generation_0shot"
 #  "coqa_0shot"
 #  "coqa_full_0shot"
-#  "squad_0shot"
+  "squad_0shot"
 
 #   TO BE IMPLEMENTED
 #  "mmlu"
