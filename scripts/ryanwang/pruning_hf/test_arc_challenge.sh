105513be616cb16a6ef343e58b75819ea6300303
ryanyxw <ryanywan@usc.edu>
2026-02-09 15:16:17 -0800

    Update

--- scripts/ryanwang/pruning_hf/test_arc_challenge.sh
+++ scripts/ryanwang/pruning_hf/test_arc_challenge.sh
@@ -4,15 +4,16 @@
 BASE_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE
 #BASE_DIR="/root/ryanwang/phdbrainstorm/FlexMoE"
 MODELS=(
-    "twolevelbatchlb-32_1b14b_stability_prenorm_noqknorm_1121/step30995-hf"
-    "twolevelbatchlb-32_1b14b_lr-4e-3_lb-1e-1_0119/step30995-hf"
-    "twolevelbatchlb-32_1b14b_lr-4e-3_lb-1e-2_0118/step30995-hf"
+#    "twolevelbatchlb-32_1b14b_stability_prenorm_noqknorm_1121/step30995-hf"
+#    "twolevelbatchlb-32_1b14b_lr-4e-3_lb-1e-1_0119/step30995-hf"
+#    "twolevelbatchlb-32_1b14b_lr-4e-3_lb-1e-2_0118/step30995-hf"
+    "twolevelbatchlbreducedp512-32_1b14b_lr-4e-3_lb-1e-2_0207/step30995-hf"
 
-    "moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123/step30995-hf"
+#    "moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123/step30995-hf"
 #    "moe_1b14b_128experts_lb-1e-1_1217/step30995-hf"
 
-    "dense_1b_olmoe-mix_prenorm_noqknorm_1123/step30995-hf"
-    "moe_1b4b_32experts_1224/step30995-hf"
+#    "dense_1b_olmoe-mix_prenorm_noqknorm_1123/step30995-hf"
+#    "moe_1b4b_32experts_1224/step30995-hf"
     )
 
 CLUSTER="ai2/jupiter-cirrascale-2"
@@ -26,38 +27,38 @@ batch_size=32
 TASK_GROUPS_LIST=(
   ######### few-shot ##########
   # MC9 tasks
-#  "arc_easy"
-#  "arc_challenge"
-#  "boolq"
-#  "csqa"
-#  "hellaswag"
-#  "openbookqa"
-#  "piqa"
-#  "socialiqa"
-#  "winogrande"
-#  "gsm8k_generation_0shot"
-#  "gsm8k_perplexity_0shot"
-#  "coqa_0shot"
-#  "coqa_full_0shot"
-#  "squad_0shot"
+  "arc_easy"
+  "arc_challenge"
+  "boolq"
+  "csqa"
+  "hellaswag"
+  "openbookqa"
+  "piqa"
+  "socialiqa"
+  "winogrande"
+  "gsm8k_generation_0shot"
+  "gsm8k_perplexity_0shot"
+  "coqa_0shot"
+  "coqa_full_0shot"
+  "squad_0shot"
 
 #  "mmlu_biology"
-  "mmlu_business"
-  "mmlu_chemistry"
-  "mmlu_computer_science"
-  "mmlu_culture"
-  "mmlu_economics"
-  "mmlu_engineering"
-  "mmlu_geography"
-  "mmlu_health"
-  "mmlu_history"
-  "mmlu_law"
-  "mmlu_math"
-  "mmlu_other"
-  "mmlu_philosophy_cat"
-  "mmlu_physics"
-  "mmlu_politics"
-  "mmlu_psychology"
+#  "mmlu_business"
+#  "mmlu_chemistry"
+#  "mmlu_computer_science"
+#  "mmlu_culture"
+#  "mmlu_economics"
+#  "mmlu_engineering"
+#  "mmlu_geography"
+#  "mmlu_health"
+#  "mmlu_history"
+#  "mmlu_law"
+#  "mmlu_math"
+#  "mmlu_other"
+#  "mmlu_philosophy_cat"
+#  "mmlu_physics"
+#  "mmlu_politics"
+#  "mmlu_psychology"
 
 #  "synthea_zeroshot"
 
@@ -215,7 +216,7 @@ for MODEL in "${MODELS[@]}"; do
         echo "Launched evaluation for model: $MODEL, task: $TASK"
         echo "----------------------------------------"
 
-        sleep 20 # brief pause to avoid overwhelming huggingface
+        sleep 30 # brief pause to avoid overwhelming huggingface
     done
 
     echo "Completed all groups for model: $model"
