b68d90579b21b47cee993d2f49b7ab96cb90e5bf
Kevin Farhat <kevinfarhat@allenai.org>
2026-02-15 16:36:43 -0800

    updated script and legalbench task remove

--- scripts/kevinf/train/launch_pmc.sh
+++ scripts/kevinf/train/launch_pmc.sh
@@ -14,16 +14,16 @@ train_tokens_B=10  # in billions
 train_tokens_raw=$((train_tokens_B * 1000000000))
 load_path="/weka/oe-training-default/kevinf/checkpoints-new/new-kevinf-olmo3-1b-130b-dolma3-0625-150Bsample/step30995"
 
-for lr in 5e-5 ; do
+for lr in 5e-6; do
   # Construct runname from hyperparams
-  runname="olmo3-1b-${dataset}-${train_tokens_B}B-lr${lr}-warmup${warmup_fraction}"
+  runname="test-lmevaluator-olmo3-1b-${dataset}-${train_tokens_B}B-lr${lr}-warmup${warmup_fraction}"
   if [ -n "$load_path" ]; then
     runname="${runname}-ctd"
   fi
 
   python -m olmo_core.launch.beaker \
     --name $runname \
-    --gpus 8 \
+    --gpus 2 \
     --nodes 1 \
     --weka=oe-training-default \
     --is_private_repo \
@@ -33,7 +33,8 @@ for lr in 5e-5 ; do
     --cluster ai2/jupiter \
     --preemptible \
     --allow-dirty \
-    --env-secret "GITHUB_TOKEN=KEVINF_GITHUB_TOKEN" "WANDB_API_KEY=KEVINF_WANDB_API_KEY" "BEAKER_TOKEN=KEVINF_BEAKER_TOKEN" \
+    --env-secret "GITHUB_TOKEN=KEVINF_GITHUB_TOKEN" "WANDB_API_KEY=KEVINF_WANDB_API_KEY" "BEAKER_TOKEN=KEVINF_BEAKER_TOKEN" "AWS_ACCESS_KEY_ID=KEVINF_AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY=KEVINF_AWS_SECRET_ACCESS_KEY" \
+    --env "S3_PROFILE=" \
     -- src/scripts/kevinf/train/OLMo3-1B.py \
     $runname \
     --save-folder="/weka/oe-training-default/kevinf/checkpoints/${runname}/" \
@@ -42,7 +43,10 @@ for lr in 5e-5 ; do
     --trainer.hard_stop="{value: ${train_tokens_raw}, unit: tokens}" \
     --trainer.callbacks.downstream_evaluator.eval_interval=100 \
     --dataset.mix=$dataset \
-    --eval-mix $dataset \
+    --dataset.mix_base_dir=s3://ai2-llm \
+    --trainer.callbacks.lm_evaluator.eval_dataset.mix=$dataset \
+    --trainer.callbacks.lm_evaluator.eval_dataset.mix_base_dir=s3://ai2-llm \
+    --trainer.callbacks.lm_evaluator.enabled=true \
     --train_module.optim.lr=$lr \
     ${load_path:+--load_path=$load_path} \
     --train_module.scheduler.warmup_fraction=$warmup_fraction \
