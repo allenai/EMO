88a1ba4916027dc37f2a8a2e3a49b183293e3027
Kevin Farhat <kevinfarhat@allenai.org>
2026-02-17 15:24:13 -0800

    fix medmcqa

--- src/offline_evals/tasks/medmcqa.py
+++ src/offline_evals/tasks/medmcqa.py
@@ -19,7 +19,7 @@ class MedMCQARC(OriginalMedMCQA):
             "index": index,
             "query": query,
             "choices": choices,
-            "gold": int(doc["cop"]) - 1,  # cop is 1-indexed (1=A,2=B,3=C,4=D)
+            "gold": int(doc["cop"]),  # cop is 0-indexed (0=A,1=B,2=C,3=D)
         }
         return out_doc
 
@@ -41,6 +41,6 @@ class MedMCQAMC(OriginalMedMCQAMC):
             "index": index,
             "query": query,
             "choices": choice_labels,
-            "gold": int(doc["cop"]) - 1,  # cop is 1-indexed (1=A,2=B,3=C,4=D)
+            "gold": int(doc["cop"]),  # cop is 0-indexed (0=A,1=B,2=C,3=D)
         }
         return out_doc
