bdbf131d523f5e92fdf8e76f80024d0f71b9c6f8
ryanyxw <ryanywan@usc.edu>
2026-03-02 15:38:48 -0800

    update

--- scripts/ryanwang/pruning_hf/launch_pruning_hf.sh
+++ scripts/ryanwang/pruning_hf/launch_pruning_hf.sh
@@ -5,11 +5,11 @@ BASE_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE
 #BASE_DIR="/root/phdbrainstorm/FlexMoE"
 MODELS=(
 #    "twolevelbatchlbreducedp512sharedexp1-32_1b14b_lr-4e-3_lb-1e-1_0211/step30995-hf"
-#    "twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0301/step30995-hf"
+    "twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0301/step30995-hf"
 #    "twolevelbatchlbreducedp512sharedexp1-32_1b14b_lr-4e-3_lb-1e-2_0213/step30995-hf"
 #    "dense_1b_lr-4e-3_0213/step30995-hf"
 #    "moereducedp256_1b4b_lr-4e-3_lb-1e-1_0212/step30995-hf"
-    "moereducedp512_1b14b_lr-4e-3_lb-1e-1_0211/step30995-hf"
+#    "moereducedp512_1b14b_lr-4e-3_lb-1e-1_0211/step30995-hf"
 
 #    "moe_1b14b_128experts_olmoe-mix_130B_prenorm_noqknorm_1123/step30995-hf"
 #    "moe_1b14b_128experts_lb-1e-1_1217/step30995-hf"
@@ -33,10 +33,11 @@ model_type=hf
 #               "layerwise"           -- greedy layer-by-layer pruning (each layer conditioned
 #                                        on already-pruned earlier layers)
 #               "layerwise_variable"  -- greedy layerwise with per-layer keep-k schedule
-PRUNING_MODE="global"
+PRUNING_MODE="layerwise"
 
 num_epochs=1
-PRUNE_KEEP_K_VALUES=(8 16 32 64)
+#PRUNE_KEEP_K_VALUES=(8 16 32 64)
+PRUNE_KEEP_K_VALUES=(64)
 batch_size=32
 
 # --- Layerwise-variable settings (only used when PRUNING_MODE="layerwise_variable") ---
@@ -71,9 +72,9 @@ TASK_GROUPS_LIST=(
 #  "mmlu_culture"
 #  "mmlu_economics"
 #  "mmlu_engineering"
-#  "mmlu_geography"
+  "mmlu_geography"
 #  "mmlu_health"
-  "mmlu_history"
+#  "mmlu_history"
 #  "mmlu_law"
 #  "mmlu_math"
 #  "mmlu_other"
