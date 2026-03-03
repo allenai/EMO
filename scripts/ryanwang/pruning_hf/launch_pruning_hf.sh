eeba801b0de9933d3aaedefa7eb8d58b29a35ce4
ryanyxw <ryanywan@usc.edu>
2026-03-02 19:26:46 -0800

    begin test eval densefirst model

--- scripts/ryanwang/pruning_hf/launch_pruning_hf.sh
+++ scripts/ryanwang/pruning_hf/launch_pruning_hf.sh
@@ -5,7 +5,8 @@ BASE_DIR=/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE
 #BASE_DIR="/root/phdbrainstorm/FlexMoE"
 MODELS=(
 #    "twolevelbatchlbreducedp512sharedexp1-32_1b14b_lr-4e-3_lb-1e-1_0211/step30995-hf"
-    "twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0301/step30995-hf"
+#    "twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0301/step30995-hf"
+    "twolevelbatchlbreducedp512sharedexp1densefirst-32_1b14b_lr-4e-3_lb-1e-1_0227/step30995-hf"
 #    "twolevelbatchlbreducedp512sharedexp1-32_1b14b_lr-4e-3_lb-1e-2_0213/step30995-hf"
 #    "dense_1b_lr-4e-3_0213/step30995-hf"
 #    "moereducedp256_1b4b_lr-4e-3_lb-1e-1_0212/step30995-hf"
@@ -37,7 +38,7 @@ PRUNING_MODE="layerwise"
 
 num_epochs=1
 #PRUNE_KEEP_K_VALUES=(8 16 32 64)
-PRUNE_KEEP_K_VALUES=(64)
+PRUNE_KEEP_K_VALUES=(32)
 batch_size=32
 
 # --- Layerwise-variable settings (only used when PRUNING_MODE="layerwise_variable") ---
@@ -50,7 +51,7 @@ KEEP_K_PER_LAYER="128,128,32,32,32,32,32,32,32,32,32,32,32,32,32,32"
 TASK_GROUPS_LIST=(
   ######### few-shot ##########
   # MC9 tasks
-#  "arc_easy"
+  "arc_easy"
 #  "arc_challenge"
 #  "boolq"
 #  "csqa"
@@ -74,7 +75,7 @@ TASK_GROUPS_LIST=(
 #  "mmlu_engineering"
 #  "mmlu_geography"
 #  "mmlu_health"
-  "mmlu_history"
+#  "mmlu_history"
 #  "mmlu_law"
 #  "mmlu_math"
 #  "mmlu_other"
