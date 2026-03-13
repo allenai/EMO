d6b3ea2d43841fe44aa85cd7df61b2a3b339e753
ryanyxw <ryanywan@usc.edu>
2026-03-12 22:09:50 -0700

    update

--- scripts/ryanwang/pruning_hf/launch_pruning_hf.sh
+++ scripts/ryanwang/pruning_hf/launch_pruning_hf.sh
@@ -54,38 +54,38 @@ KEEP_K_PER_LAYER="128,128,32,32,32,32,32,32,32,32,32,32,32,32,32,32"
 TASK_GROUPS_LIST=(
   ######### few-shot ##########
 #   MC9 tasks
-  "arc_easy"
-  "arc_challenge"
-  "boolq"
-  "csqa"
-  "hellaswag"
-  "openbookqa"
-  "piqa"
-  "socialiqa"
-  "winogrande"
+#  "arc_easy"
+#  "arc_challenge"
+#  "boolq"
+#  "csqa"
+#  "hellaswag"
+#  "openbookqa"
+#  "piqa"
+#  "socialiqa"
+#  "winogrande"
 #  "gsm8k_generation_0shot"
 #  "gsm8k_perplexity_0shot"
 #  "coqa_0shot"
 #  "coqa_full_0shot"
 #  "squad_0shot"
 
-  "mmlu_biology"
-  "mmlu_business"
-  "mmlu_chemistry"
-  "mmlu_computer_science"
-  "mmlu_culture"
-  "mmlu_economics"
-  "mmlu_engineering"
-  "mmlu_geography"
+#  "mmlu_biology"
+#  "mmlu_business"
+#  "mmlu_chemistry"
+#  "mmlu_computer_science"
+#  "mmlu_culture"
+#  "mmlu_economics"
+#  "mmlu_engineering"
+#  "mmlu_geography"
   "mmlu_health"
-  "mmlu_history"
-  "mmlu_law"
-  "mmlu_math"
-  "mmlu_other"
-  "mmlu_philosophy_cat"
-  "mmlu_physics"
-  "mmlu_politics"
-  "mmlu_psychology"
+#  "mmlu_history"
+#  "mmlu_law"
+#  "mmlu_math"
+#  "mmlu_other"
+#  "mmlu_philosophy_cat"
+#  "mmlu_physics"
+#  "mmlu_politics"
+#  "mmlu_psychology"
 
 #  "synthea_zeroshot"
 
@@ -220,7 +220,7 @@ for MODEL in "${MODELS[@]}"; do
                 "
             echo "Launched evaluation for model: $model, task: $TASK"
             echo "----------------------------------------"
-            sleep 30 # brief pause to avoid overwhelming huggingface
+            sleep 300 # brief pause to avoid overwhelming huggingface
             continue
         fi
 
@@ -363,7 +363,7 @@ for MODEL in "${MODELS[@]}"; do
         echo "Launched evaluation for model: $MODEL, task: $TASK"
         echo "----------------------------------------"
 
-        sleep 30 # brief pause to avoid overwhelming huggingface
+        sleep 300 # brief pause to avoid overwhelming huggingface
     done
 
     echo "Completed all tasks for model: $MODEL, keep-k: $prune_keep_k"
