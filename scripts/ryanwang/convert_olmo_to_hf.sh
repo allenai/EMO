3d4df34c143ce2cb0c76eaf59baadfde86be2551
ryanyxw <ryanywan@usc.edu>
2026-03-18 23:26:01 -0700

    update

--- scripts/ryanwang/convert_olmo_to_hf.sh
+++ scripts/ryanwang/convert_olmo_to_hf.sh
@@ -22,6 +22,7 @@ MODELS=(
 #    "twolevelbatchlbreducedp512sharedexp1-32_1b14b_lr-4e-3_lb-1e-1_0211/step30995"
 #    "twolevelbatchlbreducedp512sharedexp1densefirst-32_1b14b_lr-4e-3_lb-1e-1_0227/step30995"
 #    "twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0301/step30995"
+    "twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_1T_0313/step238419"
 #    "twolevelbatchlbreducedp512sharedexp2randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0305/step30995"
 #    "twolevelbatchlbreducedp512sharedexp4c2-32_1b14b_lr-4e-3_lb-1e-2_sharelb-1e-2_0214/step30995"
 #    "twolevelbatchlbreducedp512sharedexp4c2-32_1b14b_lr-4e-3_lb-1e-1_sharelb-1e-1_0214/step30995"
@@ -30,7 +31,7 @@ MODELS=(
 #    "moereducedp256_1b4b_lr-4e-3_lb-1e-1_0212/step30995"
 #    "moereducedp512sharedexp1_1b4b_lr-4e-3_lb-1e-1_0308/step30995"
 #    "moereducedp512_1b14b_lr-4e-3_lb-1e-1_0211/step30995"
-    "moereducedp512sharedexp1_1b14b_lr-4e-3_lb-1e-1_0308/step30995"
+#    "moereducedp512sharedexp1_1b14b_lr-4e-3_lb-1e-1_0308/step30995"
 
 
 
