2c1b7c3a819350b7ae8516e6352c016ae685357d
Akshita Bhagia <akshita23bhagia@gmail.com>
2026-03-24 10:54:27 -0700

    update

--- scripts/akshitab/extension-experiments/launch_beaker_evals.sh
+++ scripts/akshitab/extension-experiments/launch_beaker_evals.sh
@@ -120,7 +120,11 @@ MODELS=(
     # moereducedp512sharedexp1_132experts_4trained_code_mix_init_top2_average_noise_10B_lr_4e-4/step2385-hf
 
     # moereducedp512sharedexp1_1b14b_128experts_127_123_1_76_trained_code_10B_lr_4e-4/step2385-hf
-    moereducedp512sharedexp1_1b14b_128experts_127_69_30_3_trained_math_10B_lr_4e-4/step2385-hf
+    # moereducedp512sharedexp1_1b14b_128experts_127_69_30_3_trained_math_10B_lr_4e-4/step2385-hf
+    # twolevel_1b14b_128experts_63_26_6_19_trained_code_10B_lr_4e-4/step2385-hf
+    twolevel_1b14b_128experts_99_0_42_88_trained_math_10B_lr_4e-4/step2385-hf
+    moereducedp512sharedexp1_132experts_4trained_math_init_top2_average_10B_lr_4e-4/step2385-hf
+    moereducedp512sharedexp1_132experts_4trained_code_mix_init_top2_average_noise_10B_lr_4e-4/step2385-hf
 )
 
 BASE_OUTPUT_DIR="s3://ai2-sewonm/akshitab/mose/evals/extensions"
