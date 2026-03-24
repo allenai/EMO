c4d6edcbdff7c15015cac3e2132f81f482a9c815
Akshita Bhagia <akshita23bhagia@gmail.com>
2026-03-23 22:23:52 -0700

    add selective training scripts for twolevel

---
+++ scripts/akshitab/extension-experiments/twolevel_moe/selective/selective_4code_expert_init_top2_average_noise.sh
@@ -0,0 +1,43 @@
+# Top 4 code experts (excluding shared expert 127) from router analysis
+# python src/scripts/eval/router_analysis.py --router-files router_evals/twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0301_step30995-hf/task-mbpp-router.jsonl
+EXPERTS_TO_TRAIN=63,26,6,19
+
+TOTAL_EXPERTS=128
+
+BASE_MODEL_PATH="/weka/oe-training-default/ryanwang/phdbrainstorm/FlexMoE/models/twolevelbatchlbreducedp512sharedexp1randpool-8-128eval32_1b14b_lr-4e-3_lb-1e-1_0301/step30995"
+
+NUM_BILLION_TOKENS=10
+NUM_TOKENS=$((NUM_BILLION_TOKENS * 1000000000))
+
+LR=4e-4 #4e-4  # 4e-3, #4e-5
+
+RUN_NAME="twolevel_1b14b_${TOTAL_EXPERTS}experts_${EXPERTS_TO_TRAIN//,/_}_trained_code_${NUM_BILLION_TOKENS}B_lr_${LR}"
+
+echo $RUN_NAME
+
+python -m olmo_core.launch.beaker \
+  --name ${RUN_NAME} \
+	--gpus 8 \
+  --nodes 4 \
+	--weka=oe-training-default \
+  --shared-filesystem \
+	--workspace ai2/flex2 \
+	--cluster ai2/jupiter \
+	--preemptible \
+	--allow-dirty \
+	--priority urgent \
+	--env-secret "GITHUB_TOKEN=AKSHITAB_GITHUB_TOKEN" "WANDB_API_KEY=AKSHITAB_WANDB_API_KEY" "BEAKER_TOKEN=AKSHITAB_BEAKER_TOKEN" "AWS_ACCESS_KEY_ID=RYAN_AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY=RYAN_AWS_SECRET_ACCESS_KEY" "HF_TOKEN=RYAN_HF_TOKEN" \
+	-- src/scripts/akshitab/add_finegrained_expert/train_selected_experts.py \
+    ${RUN_NAME} \
+		--trainer.load_path="${BASE_MODEL_PATH}/model_and_optim" \
+		--save-folder="/weka/oe-training-default/akshitab/FlexMoE/models/${RUN_NAME}" \
+		--dataset.mix=code_mix \
+		--work-dir="/weka/oe-training-default/akshitab/dataset-cache" \
+		--trainer.max_duration="{value: ${NUM_TOKENS}, unit: tokens}" \
+		--trainer.callbacks.wandb="{enabled: true, entity: akshitab, project: olmoe-modular, name: ${RUN_NAME}, tags: [extension]}" \
+		--dataset.instance_filter_config='{repetition_max_period: 13, repetition_min_period: 1, repetition_max_count: 32}' \
+		--model.block.feed_forward_moe.lb_loss_weight=1e-2 \
+        --train_module.scheduler.warmup_fraction=0.1 \
+        --lr=${LR} \
+        --base-model-config="${BASE_MODEL_PATH}" \
+        --experts-to-train=${EXPERTS_TO_TRAIN}
